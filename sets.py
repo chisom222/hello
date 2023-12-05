a8ca659fcf9262cb1df733bfebee2fe1448a3f3c
Nwagbo Chisom Godspower <nwagbochisom031@hotmail.com>
2023-12-05 11:50:01 +0100

    Add sets

---
+++ sets.py
@@ -0,0 +1,15 @@
+# Create an Empty set
+s = set()
+
+# Add to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(6)
+s.add(3)
+s.remove(2)
+print(s)
+
+print(f"This set has {len(s)} element")
